d2cb7a8307a96a3299dad90bd381f10eb9e0678d
Ramil <tlsmscience@gmail.com>
2024-07-10 13:38:55 +0500

    Обозначил catalog в главном urls.py

--- Сatering/urls.py
+++ Сatering/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_menu.urls', namespace='main_menu')),
+    path('catalog/', include('dishes.urls', namespace='dishes')),
 ]
