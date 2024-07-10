d4f95808b451c7c0badfecbf91ad74f73754bb4c
Ramil <tlsmscience@gmail.com>
2024-07-10 13:32:08 +0500

    Добавил первое представление для каталога и html файл к этому представилению

--- dishes/views.py
+++ dishes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def catalog(request):
+    data = {
+        'title': 'Каталог',
+    }
+    return render(
+        request,
+        'dishes/catalog.html',
+        context=data,
+    )
