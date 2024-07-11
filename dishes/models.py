0e7d69c3dedc27066c2d4ae812a7532e68fc75a6
Ramil <tlsmscience@gmail.com>
2024-07-11 17:11:38 +0500

    Создал клас Products

--- dishes/models.py
+++ dishes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Category(models.Model):
+class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
@@ -8,3 +8,17 @@ class Category(models.Model):
         db_table = 'category'
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='dish_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+
+    category = models.ForeignKey(to=Categories, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
