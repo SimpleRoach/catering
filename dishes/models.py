6ba7318f2d976e14cc5a2a6ee239d9f39262ebaa
Ramil <tlsmscience@gmail.com>
2024-07-11 16:19:56 +0500

    Создал класс категорий в models.py

--- dishes/models.py
+++ dishes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категории'
+        verbose_name_plural = verbose_name
