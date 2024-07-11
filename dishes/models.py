f8fe2c8dab28afe2a148de6937a396130ae256f8
Ramil <tlsmscience@gmail.com>
2024-07-11 16:31:39 +0500

    Дописал класс категорий

--- dishes/models.py
+++ dishes/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
     class Meta:
         db_table = 'category'
-        verbose_name = 'Категории'
-        verbose_name_plural = verbose_name
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
