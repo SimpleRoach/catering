f866936da598e9b4961bd0ed78d87b217e531bfe
Ramil <tlsmscience@gmail.com>
2024-07-13 11:33:19 +0500

    Добавил условие дебага в главном urls.py

--- Сatering/urls.py
+++ Сatering/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from Сatering import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_menu.urls', namespace='main_menu')),
     path('catalog/', include('dishes.urls', namespace='dishes')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
