7ec198a3eaa3f76edf157a185ffa3d8cfd0462f3
Ramil <tlsmscience@gmail.com>
2024-07-13 11:54:36 +0500

    Дописал js к base.html

---
+++ static/js/base.js
@@ -0,0 +1,20 @@
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("menuDropdownMenu");
+    if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+    } else {
+        dropdownMenu.style.display = "block";
+    }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
