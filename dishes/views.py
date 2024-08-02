647b11ccc74f7ceef53ab18390d07bcd7b0db5ca
Ramil <tlsmscience@gmail.com>
2024-08-02 12:20:32 +0500

    Подготовка к написанию нового класса представлений блюд

--- dishes/views.py
+++ dishes/views.py
@@ -4,16 +4,19 @@ from django.views.generic import ListView
 from .models import Categories
 from .utils import DataMixin
 
-
 # Create your views here.
 class CategoryCatalogListView(DataMixin, ListView):
     model = Categories
     template_name = 'dishes/category_catalog.html'
     context_object_name = 'categories'
     title_page = 'Каталог'
-    # slug_url_kwarg = 'post_slug'
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return self.get_mixin_context(context, title=self.title_page)
 
+
+class DishCatalogListView(DataMixin, ListView):
+    # slug_url_kwarg = 'post_slug'
+    pass
